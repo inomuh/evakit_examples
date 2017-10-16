a5b9be609271cb1a2c3810e188beee9c8f8c1990
bdrhn9 <mail@bedirhan.org>
2017-10-16 15:57:43 +0300

    added gui

--- scripts/evakit.py
+++ scripts/evakit.py
@@ -5,10 +5,11 @@ from sensor_msgs.msg import Range
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Bool
-
+from im_msgs.msg import Battery
 class ir:
     def __init__(self,id_no):
         self.id_no = id_no
+        self.count=0
         self.range_value = float(0)
         self.max_range = float (0)
         self.min_range = float (0)
@@ -18,7 +19,8 @@ class ir:
         self.range_value = msg.range
         self.max_range = msg.max_range
         self.min_range =msg.min_range
-
+        if((self.range_value >= 0.05) and (self.range_value <=0.75) and self.count<=4):
+            self.count=self.count+1
     def get_range(self):
         return self.range_value
 
@@ -27,10 +29,15 @@ class ir:
 
     def get_max_range(self):
         return self.max_range
-
+    def is_it_work(self):
+        if (self.count >= 3):
+            return True
+        else:
+            return False
 class sonar:
     def __init__(self,id_no):
         self.id_no = id_no
+        self.count= 0
         self.range_value = float (0)
         self.max_range = float (0)
         self.min_range = float (0)
@@ -40,6 +47,13 @@ class sonar:
         self.range_value = msg.range
         self.max_range = msg.max_range
         self.min_range =msg.min_range
+        if((self.range_value >= 0.05) and (self.range_value <=4.95) and self.count<=4):
+            self.count=self.count+1
+    def is_it_work(self):
+        if (self.count >= 3):
+            return True
+        else:
+            return False
 
     def get_range(self):
         return self.range_value
@@ -54,6 +68,9 @@ class odom:
     def __init__(self):
         self.pos_x = float (0)
         self.pos_y = float (0)
+        self.pos_z = float (0)
+        self.ori_z = float (0)
+        self.ori_w = float (0)
         self.vel_x = float (0)
         self.vel_z = float (0)
         self.sub = rospy.Subscriber('odom', Odometry, self.odom_callback)
@@ -61,8 +78,12 @@ class odom:
     def odom_callback(self, msg):
         self.pos_x = msg.pose.pose.position.x
         self.pos_y = msg.pose.pose.position.y
+        self.pos_z = msg.pose.pose.position.z
         self.vel_x = msg.twist.twist.linear.x
         self.vel_z = msg.twist.twist.angular.z
+        self.ori_z = msg.pose.pose.orientation.z
+        self.ori_w = msg.pose.pose.orientation.w
+
 
     def get_pos_x(self):
         return self.pos_x
@@ -70,18 +91,27 @@ class odom:
     def get_pos_y(self):
         return self.pos_y
 
+    def get_pos_z(self):
+        return self.pos_z
+
     def get_vel_x(self):
         return self.vel_x
 
     def get_vel_z(self):
         return self.vel_z
 
+    def get_ori_z(self):
+        return self.ori_z
+
+    def get_ori_w(self):
+        return self.ori_w
+
 class motor:
     def __init__(self):
         self.speed = Twist()
         self.speed.linear.x = float(0)
         self.speed.angular.z = float(0)
-        self.pub = rospy.Publisher('cmd_vel',Twist, queue_size=5)
+        self.pub = rospy.Publisher('cmd_vel',Twist, queue_size=100)
         self.pub.publish(self.speed)
 
     def set_linear_speed(self, linear_speed):
@@ -110,6 +140,17 @@ class bumper:
 
     def get_state(self):
         return self.state.data
+
+class battery:
+    def __init__(self):
+        self.voltage = 0.0
+        self.sub=rospy.Subscriber('battery',Battery,self.bumper_callback)
+
+    def bumper_callback(self, msg):
+        self.voltage=msg.voltage
+
+    def get_voltage(self):
+        return self.voltage
 
 
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,68 +1,189 @@
 #!/usr/bin/env python
 from evakit import *
+from Tkinter import *
+
+
+        
+
+
 
 if __name__ == '__main__':
     try:
         rospy.init_node('example_node', anonymous=True)
-        rate = rospy.Rate(10)  # 1hz
+        rate = rospy.Rate(10)  # 10hz
 
         my_bumper = bumper()
         my_ir0 = ir(0)
         my_ir1 = ir(1)
+
+
         my_sonar0 = sonar(0)
         my_sonar1 = sonar(1)
+
+
         my_motor = motor()
         my_odometer = odom()
         
-        my_motor.set_linear_speed(0.3)
-        my_motor.set_angular_speed(0.5)
-        my_motor.run()
-        #my_motor.stop() if you want to stop motors
+        #my_motor.stop() #if you want to stop motors
+        def  f_button1():
+            my_motor.set_linear_speed(0.2)
+            my_motor.set_angular_speed(0.0)
+            my_motor.run()
+        def  f_button2():
+            my_motor.set_linear_speed(-0.2)
+            my_motor.set_angular_speed(0.0)
+            my_motor.run()
+        def  f_button3():
+            my_motor.set_linear_speed(0.0)
+            my_motor.set_angular_speed(-0.4)
+            my_motor.run()
+        def  f_button4():
+            my_motor.set_linear_speed(0.0)
+            my_motor.set_angular_speed(0.4)
+            my_motor.run()
+        def  f_button5():
+            my_motor.stop()
+        
+        root = Tk()
+        root.title("EVAKIT_TEST")
+
+        IR0_range = IntVar()
+        label_IR0 = Label(root, text="IR0 range=").grid(row=0, column=0, sticky=E)
+        
+        IR1_range = IntVar()
+        label_IR1 = Label(root, text="IR1 range=").grid(row=1, column=0, sticky=E)
+        
+        Sonar0_range = IntVar()
+        label_Sonar0_range = Label(root, text="Sonar0 range=").grid(row=2, column=0, sticky=E)
+        
+        Sonar1_range = IntVar()
+        label_Sonar1_range = Label(root, text="Sonar1 range=").grid(row=3, column=0, sticky=E)
+
+        
+        state_Bumper0 = IntVar()
+        label_state_Bumper0 = Label(root, text="Bumper0 state=").grid(row=4, column=0, sticky=E)
+
+        Odometer_pos_x = IntVar()
+        Odometer_pos_y = IntVar()
+        Odometer_pos_z = IntVar()
+        Odometer_vel_x = IntVar()
+        Odometer_vel_z = IntVar()
+        Odometer_ori_z = IntVar()
+        Odometer_ori_w = IntVar()
+
+        Sonar0_ok = StringVar()
+        Sonar1_ok = StringVar()
+
+
+        ir0_ok = StringVar()
+        ir1_ok = StringVar()
+
+
+        label_Odometer_pos_x = Label(root, text="pos_x=").grid(row=5, column=0, sticky=E)
+        label_Odometer_pos_y = Label(root, text="pos_y=").grid(row=6, column=0, sticky=E)
+        label_Odometer_pos_z = Label(root, text="pos_z=").grid(row=7, column=0, sticky=E)
+        label_Odometer_vel_x = Label(root, text="vel_x=").grid(row=8, column=0, sticky=E) 
+        label_Odometer_vel_z = Label(root, text="vel_z=").grid(row=9, column=0, sticky=E)
+        label_Odometer_ori_z = Label(root, text="ori_z=").grid(row=10, column=0, sticky=E) 
+        label_Odometer_ori_w = Label(root, text="ori_w=").grid(row=11, column=0, sticky=E)
+
+        entry_IR0_range = Entry(root,textvariable=IR0_range,bd=3).grid(row=0, column=1)
+        entry_IR0_ok = Entry(root, textvariable=ir0_ok, bd=3).grid(row=0, column=2)
+
+        entry_IR1_range = Entry(root, textvariable=IR1_range,bd=3).grid(row=1, column=1)
+        entry_IR1_ok = Entry(root, textvariable=ir1_ok, bd=3).grid(row=1, column=2)
+
+        entry_Sonar0_range = Entry(root, textvariable=Sonar0_range, bd=3).grid(row=2, column=1)
+        entry_Sonar0_ok = Entry(root, textvariable=Sonar0_ok, bd=3).grid(row=2, column=2)
+
+        entry_Sonar1_range = Entry(root, textvariable=Sonar1_range, bd=3).grid(row=3, column=1)
+        entry_Sonar1_ok = Entry(root, textvariable=Sonar1_ok, bd=3).grid(row=3, column=2)
+
+        entry_state_Bumper0 = Entry(root, textvariable=state_Bumper0, bd=3).grid(row=4, column=1)
+
+        entry_Odometer_pos_x = Entry(root, textvariable=Odometer_pos_x, bd=3).grid(row=5, column=1)
+        entry_Odometer_pos_y = Entry(root, textvariable=Odometer_pos_y, bd=3).grid(row=6, column=1)
+        entry_Odometer_pos_z = Entry(root, textvariable=Odometer_pos_z, bd=3).grid(row=7, column=1)
+        entry_Odometer_vel_x = Entry(root, textvariable=Odometer_vel_x, bd=3).grid(row=8, column=1)
+        entry_Odometer_vel_z = Entry(root, textvariable=Odometer_vel_z, bd=3).grid(row=9, column=1)
+        entry_Odometer_ori_z = Entry(root, textvariable=Odometer_ori_z, bd=3).grid(row=10, column=1)
+        entry_Odometer_ori_w = Entry(root, textvariable=Odometer_ori_w, bd=3).grid(row=11, column=1)
+
+        button_1=Button(root,text="FORW",command=f_button1).grid(row=12,column=1)
+        button_2=Button(root,text="BACK",command=f_button2).grid(row=14,column=1)
+        button_3=Button(root,text="RIGHT",command=f_button3).grid(row=13,column=2)
+        button_4=Button(root,text="LEFT",command=f_button4).grid(row=13,column=0)
+        button_5=Button(root,text="STOP",command=f_button5).grid(row=13,column=1)
+        
+        my_motor.stop()
 
         while not rospy.is_shutdown():
+            
             rospy.loginfo("---------------------------------")
             # ir0 functions and their usage
             range_ir0 = my_ir0.get_range()
-            min_range_ir0 = my_ir0.get_min_range()
-            max_range_ir0 = my_ir0.get_max_range()
             rospy.loginfo("range of ir0 =" + str(range_ir0))
-            rospy.loginfo("minimum range of ir0 =" + str(min_range_ir0))
-            rospy.loginfo("maximum range of ir0" + str (max_range_ir0))
             # ir1 functions and their usage
             range_ir1 = my_ir1.get_range()
-            min_range_ir1 = my_ir1.get_min_range()
-            max_range_ir1 = my_ir1.get_max_range()
             rospy.loginfo("range of ir1 =" + str(range_ir1))
-            rospy.loginfo("minimum range of ir1 =" + str(min_range_ir1))
-            rospy.loginfo("maximum range of ir1" + str(max_range_ir1))
             # sonar0 functions and their usage
             range_sonar0 = my_sonar0.get_range()
-            min_range_sonar0 = my_sonar0.get_min_range()
-            max_range_sonar0 = my_sonar0.get_max_range()
             rospy.loginfo("range of sonar0 =" + str(range_sonar0))
-            rospy.loginfo("minimum range of sonar0 =" + str(min_range_sonar0))
-            rospy.loginfo("maximum range of sonar0" + str(max_range_sonar0))
             # sonar1 functions and their usages
             range_sonar1 = my_sonar1.get_range()
-            min_range_sonar1 = my_sonar1.get_min_range()
-            max_range_sonar1 = my_sonar1.get_max_range()
             rospy.loginfo("range of sonar1 =" + str(range_sonar1))
-            rospy.loginfo("minimum range of sonar1 =" + str(min_range_sonar1))
-            rospy.loginfo("maximum range of sonar1" + str(max_range_sonar1))
+
             # bumper function and its usage
             state_bumper0 = my_bumper.get_state()
             rospy.loginfo("state of bumper=" + str(state_bumper0))
             # odom functions and their usage
             odometer_pos_x = my_odometer.get_pos_x()
             odometer_pos_y = my_odometer.get_pos_y()
+            odometer_pos_z = my_odometer.get_pos_z()
             odometer_vel_x = my_odometer.get_vel_x()
             odometer_vel_z = my_odometer.get_vel_z()
+            odometer_ori_z = my_odometer.get_ori_z()
+            odometer_ori_w = my_odometer.get_ori_w()
             rospy.loginfo("x position of robot=" + str(odometer_pos_x))
             rospy.loginfo("y position of robot=" + str(odometer_pos_y))
+            rospy.loginfo("z position of robot=" + str(odometer_pos_z))
             rospy.loginfo("x velocity of robot=" + str(odometer_vel_x))
             rospy.loginfo("z velocity of robot=" + str(odometer_vel_z))
+            rospy.loginfo("z orientation of robot=" + str(odometer_ori_z))
+            rospy.loginfo("w orientation of robot=" + str(odometer_ori_w))
             rospy.loginfo("---------------------------------")
+    
+            
+            IR0_range.set(range_ir0)
+            IR1_range.set(range_ir1)
+
+
+            Sonar0_ok.set(str(my_sonar0.is_it_work()))
+            Sonar1_ok.set(str(my_sonar1.is_it_work()))
+
+
+            ir0_ok.set(str(my_ir0.is_it_work()))
+            ir1_ok.set(str(my_ir1.is_it_work()))
+
+            Sonar0_range.set(range_sonar0)
+            Sonar1_range.set(range_sonar1)
+            
+            
+            state_Bumper0.set(state_bumper0)
+
+            
+            Odometer_pos_x.set(odometer_pos_x)
+            Odometer_pos_y.set(odometer_pos_y)
+            Odometer_pos_z.set(odometer_pos_z)
+            Odometer_vel_x.set(odometer_vel_x)
+            Odometer_vel_z.set(odometer_vel_z)
+            Odometer_ori_z.set(odometer_ori_z)
+            Odometer_ori_w.set(odometer_ori_w)
+            #close_b = Button(root, text="CLOSE", command=quit).grid(row=8, column=2)
+            root.update()
             rate.sleep()
+            #root.mainloop()
+            
 
     except rospy.ROSInterruptException:
         pass
